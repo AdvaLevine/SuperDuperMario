84e46a7526d66986c8ddf37ed3a24f82f79cd0e2
Or Hershko <or.hershko22@gmail.com>
2024-09-19 13:43:39 +0300

    added question mark blocks to the map, with animations when idle and hit.

--- Super Duper Mario/Assets/Scripts/QuestionMarkBlock.cs
+++ Super Duper Mario/Assets/Scripts/QuestionMarkBlock.cs
@@ -1,7 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class QuestionMarkBlock : MonoBehaviour
 {
+    private Vector3 originalPosition;
+    public float jumpHeight = 0.2f;  
+    public float jumpDuration = 0.1f;  
+    
     public enum BlockEffect
     {
         HigherJump,
@@ -19,6 +24,7 @@ public class QuestionMarkBlock : MonoBehaviour
 
     private void Start()
     {
+        originalPosition = transform.position;
         _animator = GetComponent<Animator>();
         // שינוי צבע בהתאם לדרגת הקושי והאפקט
         if (GameManager.Instance.CurrentDifficulty == GameManager.Difficulty.Easy)
@@ -44,7 +50,8 @@ public class QuestionMarkBlock : MonoBehaviour
             {
                 _isHit = true;
                 _animator.SetTrigger("Hit");
-                ApplyEffect(collision.gameObject.GetComponent<PlayerController>());
+                StartCoroutine(JumpUpAndDown());
+                ApplyEffect(FindObjectOfType<PlayerController>());
             }
         }
     }
@@ -60,10 +67,10 @@ public class QuestionMarkBlock : MonoBehaviour
                 player.SetJumpForceMultiplier(0.75f);
                 break;
             case BlockEffect.AddPoints:
-                ScoreManager.Instance.AddScore(10);
+                ScoreManager.Instance.AddScore(50);
                 break;
             case BlockEffect.SubtractPoints:
-                ScoreManager.Instance.AddScore(-10);
+                ScoreManager.Instance.AddScore(-50);
                 break;
             case BlockEffect.DoubleCoins:
                 player.SetCoinMultiplier(2, 10f);
@@ -78,4 +85,35 @@ public class QuestionMarkBlock : MonoBehaviour
     {
         return blockEffect == BlockEffect.HigherJump || blockEffect == BlockEffect.AddPoints || blockEffect == BlockEffect.DoubleCoins;
     }
+    
+    IEnumerator JumpUpAndDown()
+    {
+        // הזז את הבלוק למעלה
+        Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y + jumpHeight, transform.position.z);
+        
+        // תזוזה למעלה
+        float elapsedTime = 0;
+        while (elapsedTime < jumpDuration)
+        {
+            transform.position = Vector3.Lerp(originalPosition, targetPosition, (elapsedTime / jumpDuration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // וודא שהגענו למיקום העליון
+        transform.position = targetPosition;
+
+        // תזוזה חזרה למיקום המקורי
+        elapsedTime = 0;
+        while (elapsedTime < jumpDuration)
+        {
+            transform.position = Vector3.Lerp(targetPosition, originalPosition, (elapsedTime / jumpDuration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // החזר למיקום המקורי
+        transform.position = originalPosition;
+    }
+    
 }
